f36f3f72e3cd48f97e4ef62a95a286d72e0c6855
aaroldpaul <93167655+aaroldpaul@users.noreply.github.com>
2022-03-18 12:16:13 +0800

    oks

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4!n5$@b&e5d=8m5i3y@f1in=yrsblk!%#!xqmt*gmkk#--^8lc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
